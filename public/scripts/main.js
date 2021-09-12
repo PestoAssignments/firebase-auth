3e3e9ca07d715504bdf942c4f0ecdb7e212339f2
Gurdeep <gs22111994@gmail.com>
2021-09-12 16:04:40 -0400

    update gitignore

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,13 +2,6 @@ const quotes = document.getElementById('quotes');
 const error = document.getElementById('error');
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCyxCfuhPvCsBuRNh9iRzLp5LxhFU6UfIg",
-    authDomain: "food-6510f.firebaseapp.com",
-    projectId: "food-6510f",
-    storageBucket: "food-6510f.appspot.com",
-    messagingSenderId: "42073407139",
-    appId: "1:42073407139:web:018ae7c6b7136105614916",
-    measurementId: "G-M2NZ044L73"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
